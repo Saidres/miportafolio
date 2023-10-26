d417f9135bb22c88d24ef24210ebc0201cac3e0f
Santiago Chamorro <santiago.chamorro@campusucc.edu.co>
2023-10-26 07:48:18 -0500

    [Fix] logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Logo from '../assets/Logo.png';
+import Logo from "../assets/logo.png";
 import { buttonStyle } from '../data/data';
 
 const Toggle: React.FC = () => {
